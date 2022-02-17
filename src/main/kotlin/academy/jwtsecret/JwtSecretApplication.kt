24afcc6812fa51d608fefa9fe7500ae4ce247b39
Luccas Pereira <Luccas.lpn@outlook.com>
2022-02-17 20:22:19 -0300

    Started Project

---
+++ src/main/kotlin/academy/jwtsecret/JwtSecretApplication.kt
@@ -0,0 +1,11 @@
+package academy.jwtsecret
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JwtSecretApplication
+
+fun main(args: Array<String>) {
+	runApplication<JwtSecretApplication>(*args)
+}
---
+++ src/test/kotlin/academy/jwtsecret/JwtSecretApplicationTests.kt
@@ -0,0 +1,13 @@
+package academy.jwtsecret
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtSecretApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
