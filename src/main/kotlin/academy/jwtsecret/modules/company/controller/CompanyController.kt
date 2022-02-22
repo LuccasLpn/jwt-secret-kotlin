93f057a036e1f8e1b10a0bb879a04a421762524b
LuccasLpn <luccas.lpn@outlook.com>
2022-02-22 20:13:17 -0300

    Update Company

--- src/main/kotlin/academy/jwtsecret/modules/company/controller/CompanyController.kt
+++ src/main/kotlin/academy/jwtsecret/modules/company/controller/CompanyController.kt
@@ -2,13 +2,12 @@ package academy.jwtsecret.modules.company.controller
 
 import academy.jwtsecret.modules.company.domain.Company
 import academy.jwtsecret.modules.company.request.CompanyPost
+import academy.jwtsecret.modules.company.request.CompanyPut
 import academy.jwtsecret.modules.company.service.CompanyService
 import lombok.RequiredArgsConstructor
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["/api/company"])
@@ -16,8 +15,16 @@ import org.springframework.web.bind.annotation.RestController
 class CompanyController (val companyService: CompanyService){
 
 
+    @PostMapping(path = ["/save"])
     fun save(@RequestBody companyPost: CompanyPost): ResponseEntity<Company>{
         val saveCompany = companyService.save(companyPost)
         return ResponseEntity(saveCompany, HttpStatus.CREATED)
     }
+
+    @PutMapping(path = ["/update"])
+    fun update(@RequestBody companyPut: CompanyPut): ResponseEntity<Company>{
+        val updateCompany = companyService.update(companyPut)
+        return ResponseEntity(updateCompany, HttpStatus.OK)
+    }
+
 }
--- src/main/kotlin/academy/jwtsecret/modules/company/service/CompanyService.kt
+++ src/main/kotlin/academy/jwtsecret/modules/company/service/CompanyService.kt
@@ -4,6 +4,7 @@ import academy.jwtsecret.modules.company.domain.Company
 import academy.jwtsecret.modules.company.mapper.CompanyMapper
 import academy.jwtsecret.modules.company.repository.CompanyRepository
 import academy.jwtsecret.modules.company.request.CompanyPost
+import academy.jwtsecret.modules.company.request.CompanyPut
 import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
 
@@ -16,4 +17,23 @@ class CompanyService (val companyRepository: CompanyRepository){
         val save = companyRepository.save(companyPostSave)
         return save;
     }
+
+    fun update(companyPut: CompanyPut): Company{
+        val findById = companyPut.id?.let { findById(it) }
+        val toSavedPut = CompanyMapper.INSTACE.toPut(companyPut)
+        toSavedPut.id = findById?.id
+        val savedPut = companyRepository.save(toSavedPut)
+        return savedPut
+    }
+
+
+
+    fun findById(id:Long): Company{
+        return companyRepository.findById(id).orElseThrow {
+            -> RuntimeException ("ID Not Found$id")
+        }
+    }
+
+
+
 }
