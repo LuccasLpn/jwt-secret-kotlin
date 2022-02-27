578be61e8d102d94a3cded5fa1d7ad31175439f4
LuccasLpn <luccas.lpn@outlook.com>
2022-02-27 14:11:32 -0300

    UserDetailsImpl

---
+++ src/main/kotlin/academy/jwtsecret/modules/user/domain/UserSS.kt
@@ -0,0 +1,23 @@
+package academy.jwtsecret.modules.user.domain
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.*
+import java.util.stream.Collectors
+
+class UserSS(private val user: User):UserDetails{
+
+    override fun getAuthorities(): Collection<GrantedAuthority?> {
+        return Arrays.stream(user.authorities!!.split(",").toTypedArray())
+            .map { role: String? ->
+                SimpleGrantedAuthority(role) }.collect(Collectors.toList())
+    }
+
+    override fun getPassword() = user.password
+    override fun getUsername() = user.username
+    override fun isAccountNonExpired() = true
+    override fun isAccountNonLocked() = true
+    override fun isCredentialsNonExpired() = true
+    override fun isEnabled() = true
+}
